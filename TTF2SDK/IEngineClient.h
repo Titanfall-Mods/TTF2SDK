c20af650a224555eba306a8860f0e71ede128bf2
Will Donohoe <will@mcsi.mp>
2018-04-25 18:35:14 +1000

    File loading for wilko

--- TTF2SDK/IEngineClient.h
+++ TTF2SDK/IEngineClient.h
@@ -6,8 +6,6 @@ public:
     struct VTable
     {
         void* unknown[5];
-        int(*FirstFuncOfInterest) (IEngineClient* engineClient, const char* mapName, char a3);
-        int(*SecondFuncOfInterest) (IEngineClient* engineClient, const char* mapName);
     };
 
     VTable* m_vtable;
